18674ba01164d04d43af19df9abfa65765ceb9c4
codeskyblue <codeskyblue@gmail.com>
2017-01-16 17:03:38 +0800

    add goreleaser

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # wdaproxy
+[![Powered By: GoReleaser](https://img.shields.io/badge/powered%20by-goreleaser-green.svg?style=flat-square)](https://github.com/goreleaser)
 WebDriverAgent Proxy
 
 Only can work in Mac
--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os/exec"
+	"strconv"
+)
+
+var (
+	version = "develop"
+	lisPort = 8100
 )
 
 func main() {
+	showVer := flag.Bool("v", false, "Print version")
+	flag.IntVar(&lisPort, "p", 8100, "Proxy listen port")
+	flag.Parse()
+
+	if *showVer {
+		println(version)
+		return
+	}
+
 	log.Println("program start......")
 	errC := make(chan error)
 
@@ -17,11 +33,11 @@ func main() {
 		targetURL, _ := url.Parse("http://127.0.0.1:8200")
 		httpProxy := httputil.NewSingleHostReverseProxy(targetURL)
 		http.Handle("/", httpProxy)
-		errC <- http.ListenAndServe(":8100", nil)
+		errC <- http.ListenAndServe(":"+strconv.Itoa(lisPort), nil)
 	}()
 	go func() {
 		log.Println("launch iproxy")
-		c := exec.Command("iproxy", "8200", "8100")
+		c := exec.Command("iproxy", "8200", strconv.Itoa(lisPort))
 		errC <- c.Run()
 	}()
 
