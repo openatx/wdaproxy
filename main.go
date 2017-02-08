8e277fd0640e8e0309b8a6e7f8d483395eb49b2e
codeskyblue <codeskyblue@gmail.com>
2017-02-08 14:31:58 +0800

    add index page

--- main.go
+++ main.go
@@ -17,18 +17,23 @@ var (
 	version = "develop"
 	lisPort = 8100
 	udid    string
-)
-
-func NewReverseProxyHandlerFunc(targetURL *url.URL) http.HandlerFunc {
-	httpProxy := httputil.NewSingleHostReverseProxy(targetURL)
-	return func(rw http.ResponseWriter, r *http.Request) {
-		if r.RequestURI == "/" {
-			io.WriteString(rw, `<!doctype html>
+	
+	indexContent = `<!doctype html>
 <html>
 <head>
   <meta charset="utf-8">
   <title>A static page</title>
-  <link rel="stylesheet" href="/stylesheets/main.css">
+  <style>
+  body {
+	  padding: 50px 80px;
+	  margin: 0px;
+  }
+  pre {
+	  font-family: "Courier New";
+	  padding: 10px;
+	  border: 1px solid gray;
+  }
+  </style>
 </head>
 <body>
   <h1>WDA Proxy</h1>
@@ -37,9 +42,32 @@ func NewReverseProxyHandlerFunc(targetURL *url.URL) http.HandlerFunc {
 	  <li><a href="/inspector">Inspector</a></li>
 	  <li><a href="/status">Status</a></li>
 	</ul>
+	<pre id="status"></pre>
   </div>
 </body>
-</html>`)
+<script src="//cdn.jsdelivr.net/jquery/3.1.1/jquery.min.js"></script>
+<script>
+var pre = document.getElementById("status");
+$.ajax({
+	url: "/status",
+	dataType: "json",
+	success: function(ret){
+		pre.innerHTML = JSON.stringify(ret, null, "    ");
+	},
+	error: function(xhr, status, err){
+		pre.innerHTML = xhr.status + " " + err;
+		pre.style.color = "red";
+	}
+})
+</script>
+</html>`
+)
+
+func NewReverseProxyHandlerFunc(targetURL *url.URL) http.HandlerFunc {
+	httpProxy := httputil.NewSingleHostReverseProxy(targetURL)
+	return func(rw http.ResponseWriter, r *http.Request) {
+		if r.RequestURI == "/" {
+			io.WriteString(rw, indexContent)
 			return
 		}
 		httpProxy.ServeHTTP(rw, r)
@@ -63,6 +91,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("get freeport %d", freePort)
 
 	go func() {
 		log.Printf("launch tcp-proxy, listen on %d", lisPort)
@@ -73,7 +102,10 @@ func main() {
 	}()
 	go func() {
 		log.Printf("launch iproxy, device udid(%s)", udid)
-		c := exec.Command("iproxy", strconv.Itoa(freePort), strconv.Itoa(lisPort), udid)
+		c := exec.Command("iproxy", strconv.Itoa(freePort), "8100")
+		if udid != "" {
+			c.Args = append(c.Args, udid)
+		}
 		errC <- c.Run()
 	}()
 
