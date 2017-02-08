68e1adb83426b75d47c981be51136027d568d4f9
codeskyblue <codeskyblue@gmail.com>
2017-02-08 12:59:05 +0800

    add index page

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -17,6 +18,33 @@ var (
 	lisPort = 8100
 	udid    string
 )
+
+func NewReverseProxyHandlerFunc(targetURL *url.URL) http.HandlerFunc {
+	httpProxy := httputil.NewSingleHostReverseProxy(targetURL)
+	return func(rw http.ResponseWriter, r *http.Request) {
+		if r.RequestURI == "/" {
+			io.WriteString(rw, `<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>A static page</title>
+  <link rel="stylesheet" href="/stylesheets/main.css">
+</head>
+<body>
+  <h1>WDA Proxy</h1>
+  <div>
+  	<ul>
+	  <li><a href="/inspector">Inspector</a></li>
+	  <li><a href="/status">Status</a></li>
+	</ul>
+  </div>
+</body>
+</html>`)
+			return
+		}
+		httpProxy.ServeHTTP(rw, r)
+	}
+}
 
 func main() {
 	showVer := flag.Bool("v", false, "Print version")
@@ -39,8 +67,8 @@ func main() {
 	go func() {
 		log.Printf("launch tcp-proxy, listen on %d", lisPort)
 		targetURL, _ := url.Parse("http://127.0.0.1:" + strconv.Itoa(freePort))
-		httpProxy := httputil.NewSingleHostReverseProxy(targetURL)
-		http.Handle("/", httpProxy)
+		httpProxyFunc := NewReverseProxyHandlerFunc(targetURL)
+		http.HandleFunc("/", httpProxyFunc)
 		errC <- http.ListenAndServe(":"+strconv.Itoa(lisPort), nil)
 	}()
 	go func() {
